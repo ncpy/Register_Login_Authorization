64248313eb1aeefab9021d35f6f09f37517af840
necip <43618473+ncpy@users.noreply.github.com>
2022-08-12 12:40:06 +0300

    validation with joi

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,38 @@ router.get("/test", (req,res) => {
     res.send("auth works")
 })
 
-// 5
+// 6
+//postman de {"email":"ali@g.com", "password": "123"} gibi dene
 router.post("/signup", (req,res) => {
     const { password, email } = req.body
-    console.log(password, email) 
-    //if (password.length <6)...  => use 3rd part lib. like jio/express-validator
+    const data = req.body
+    console.log("tüm log: ",validate(data))
+    console.log("value log: ",validate(data).value)
+    console.log("hata log: ",validate(data)?.error)
     res.send("auth works")
 })
+const passwordComplexity = require("joi-password-complexity")
+const Joi = require("joi")
+const validate = (data) => {
+    const schema = Joi.object().keys({
+        email: Joi.string()
+            .min(5)
+            .max(255)
+            .required()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+            .label("Email"),
+            //  "Email" must be a valid email
+            //  "Email" length must be at least 5 characters long
+        password: passwordComplexity()
+            .required()
+            .label("Password")
+            //  "Password" should be at least 8 characters long.
+            //  "Password" should contain at least 1 lower-cased letter.
+            //  "Password" should contain at least 1 upper-cased letter.
+            //  "Password" should contain at least 1 symbol.
+    })
+    return schema.validate(data)
+}
+
 
 module.exports = router
