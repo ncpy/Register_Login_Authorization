5f798c44f07f61f1340de672a642a58e228dc561
necip <43618473+ncpy@users.noreply.github.com>
2022-08-12 14:48:46 +0300

    password crypto

--- models/User.js
+++ models/User.js
@@ -11,5 +11,34 @@ const userSchema = new mongoose.Schema(
     {timestamps: true} //_id createdAt updatedAt ekleniyor
 )
 
+const passwordComplexity = require("joi-password-complexity")
+const Joi = require("joi");
+const validate = (data) => {
+    const schema = Joi.object().keys({
+        username: Joi.string()
+            .min(3)
+            .max(36)
+            .required()
+            .label("Username"),
+        email: Joi.string()
+            .min(5)
+            .max(255)
+            .required()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+            .label("Email"),
+            //  "Email" must be a valid email
+            //  "Email" length must be at least 5 characters long
+        password: passwordComplexity()
+            .required()
+            .label("Password")
+            //  "Password" should be at least 8 characters long.
+            //  "Password" should contain at least 1 lower-cased letter.
+            //  "Password" should contain at least 1 upper-cased letter.
+            //  "Password" should contain at least 1 symbol.
+    })
+    return schema.validate(data)
+}
+
+
 const User = mongoose.model("user", userSchema) // create users (yes with -s in mongoDB)
-module.exports = { User }
+module.exports = { User, validate }
--- routes/auth.js
+++ routes/auth.js
@@ -7,60 +7,50 @@ router.get("/test", (req,res) => {
 
 // 6
 //postman de {"email":"ali@g.com", "password": "123"} gibi dene
-const { User } = require("../models/User");
+const { User, validate } = require("../models/User");
+const CryptoJS = require("crypto-js")
 router.post("/signup", async(req,res) => {
     const { value, error } = validate(req.body);
     if (error) { //validation error
-        res.status(422).send(error.details[0].message);
         console.log(error)
-    } else { //validation PASS
-        console.log(value)
-        const user = await User.findOne({ email:req.body.email }) //user exist ?
-        if(user) { //user vardı
-            console.log("Bu kullanıcı zaten var")
-            return res.status(401).send("Bu kullanıcı zaten var" )
-        } else { //yeni user yani DB ye kaydet
-            try {
-                // save to MongoDB
-                const { password, email } = value // const { password, email } = validate(req.body).value
-                const newUser = new User({
-                    email: email,
-                    password: password
-                })
-                const savedUser = await newUser.save()
-                res.status(201).json(savedUser)
-                //console.log("savedUser: "+savedUser) //yukarıdakinin işini bitirmesini bekledikten sonra yazmaya çalışır
-            } catch(err){
-                console.log("err: "+err)
-                res.status(500).json(err)
-            }
-        }
+        return res.status(422).send(error.details[0].message);
+    } //hata yok yani validation PASSed
+    
+    // kullanıcı adı alınmış mı
+    const userN = await User.findOne({ username:req.body.username })
+    if(userN) {
+        console.log("Kullanıcı adı alınmış")
+        return res.status(401).send("Kullanıcı adı alınmış" )
+    } 
+
+    // email alınmış mı
+    const user = await User.findOne({ email:req.body.email }) //user exist ?
+    if(user) { //user vardı
+        console.log("Bu kullanıcı zaten var")
+        return res.status(401).send("Bu kullanıcı zaten var" )
+    }
+    
+    //yeni user yani DB ye kaydet
+    try {
+        const { password, email } = value // const { password, email } = validate(req.body).value
+        const newUser = new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            username: req.body.username,
+            email: req.body.email,
+            password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSPHRASE_SECRET).toString()
+        })
+        const savedUser = await newUser.save()
+        console.log("savedUser: "+savedUser)
+        res.status(201).json(savedUser)
+    } catch(err){
+        console.log("err: "+err)
+        res.status(500).json(err)
+    }
 
         //res.send("auth works") //dikkat et 1 req için 1 res göndrebilirsin
-    }
+    
 })
-const passwordComplexity = require("joi-password-complexity")
-const Joi = require("joi");
-const validate = (data) => {
-    const schema = Joi.object().keys({
-        email: Joi.string()
-            .min(5)
-            .max(255)
-            .required()
-            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-            .label("Email"),
-            //  "Email" must be a valid email
-            //  "Email" length must be at least 5 characters long
-        password: passwordComplexity()
-            .required()
-            .label("Password")
-            //  "Password" should be at least 8 characters long.
-            //  "Password" should contain at least 1 lower-cased letter.
-            //  "Password" should contain at least 1 upper-cased letter.
-            //  "Password" should contain at least 1 symbol.
-    })
-    return schema.validate(data)
-}
 
 
 module.exports = router
