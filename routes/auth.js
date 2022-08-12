6e28e270ba601996bb36b3d58f7c0776f77f41fa
necip <43618473+ncpy@users.noreply.github.com>
2022-08-12 12:12:56 +0300

    localhost:5000/auth/signup POST

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,10 @@ router.get("/test", (req,res) => {
     res.send("auth works")
 })
 
-
+// 4
+// postmanda localhost:5000/auth/signup POST yapılırsa çıktı görünür
+router.post("/signup", (req,res) => {
+    res.send("auth works")
+})
 
 module.exports = router
