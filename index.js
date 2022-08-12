3debcb259ad72c5b9b88f7af9f4163928459be15
necip <43618473+ncpy@users.noreply.github.com>
2022-08-12 11:57:53 +0300

    localhost:5000/ ekran ilk deneme

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
 
+//localhost:5000/ adresine gidince ekranda görünen;
+app.get("/", (req,res) => {
+    res.send("We did it")
+})
+
 app.listen(5000, () => {
     console.log("backend çalışıyor.")
 })
